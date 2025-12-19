f53933b294197a2f090f7ab5109922f17badf159
Svitlana Frolova <sfrol91@gmail.com>
2025-12-19 16:21:01 +0100

    Adding classes and 1 method in Cart

---
+++ Cart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOnlineShop
+{
+    public class Cart
+    {
+        public string productTitle;
+        public DateTime timeOrder;
+        public TimeSpan timeDelivery;
+        public Client user;
+        public string commentsCurrier;
+        public Dictionary<Product, double> order;
+
+        public Cart(string productTitle, DateTime timeOrder, TimeSpan timeDelivery, Client user, string commentsCurrier)
+        {
+            this.productTitle = productTitle;
+            this.timeOrder = timeOrder;
+            this.timeDelivery = timeDelivery;
+            this.user = user;
+            this.commentsCurrier = commentsCurrier;
+            this.order = new Dictionary<Product, double>();
+        }
+        //+ CalculationSum(double) : double
+        public void AddProduct(Product product)
+        {
+            order.Add(product, 1);
+        }
+    }
+}
---
+++ ProductList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOnlineShop
+{
+    public class ProductList
+    {
+        public Dictionary<Product, double> productData;
+
+        public ProductList(Dictionary<Product, double> productData)
+        {
+            this.productData = productData;
+        }
+    }
+}
